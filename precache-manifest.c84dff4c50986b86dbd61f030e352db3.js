124d3f2779e88ef368feaf6870128dee95dfb5c2
Kyle Case <casekw@gmail.com>
2019-06-22 07:30:08 -0500

    Updates

---
+++ precache-manifest.c84dff4c50986b86dbd61f030e352db3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a1c0b8ac6343e05449e98222d7882e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "745b002591ac8f6de642",
+    "url": "/static/css/main.fdf52b7f.chunk.css"
+  },
+  {
+    "revision": "845d26471f9e6a29fb8b",
+    "url": "/static/js/2.a0e3ca9a.chunk.js"
+  },
+  {
+    "revision": "745b002591ac8f6de642",
+    "url": "/static/js/main.5cedecf1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
